0c1b82a8668f605749ca96132b9ca0a8c85397a1
Venkatesh <61664017+venkey83@users.noreply.github.com>
2022-08-29 16:34:03 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,21 @@
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.ec2.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.ec2.public_ip
+}
+
+output "instance_private_ip" {
+  description = "Private IP address of the EC2 instance"
+  value       = aws_instance.ec2.private_ip
+}
+
+
+output "vpc_id" {
+  description = "ID of the vpc"
+  value       = aws_vpc.vpc.id
+
+}
