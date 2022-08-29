b2fce5a8ce5be0a3e648fe69d54eec62ab401fc0
Venkatesh <61664017+venkey83@users.noreply.github.com>
2022-08-29 17:19:06 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_vpc" "vpc" {
 cidr_block = "${var.vpc_cidr}"
 enable_dns_hostnames = "${var.dnshostname}"
@@ -115,4 +116,4 @@ count = "2"
 
 #resource "aws_security_group" "sg"
 
-
+*/
