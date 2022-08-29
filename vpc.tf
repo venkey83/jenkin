6c0049d4be970383b7f67cb30901853a36be22e4
Venkatesh <61664017+venkey83@users.noreply.github.com>
2022-08-29 17:20:00 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-/*
+
 resource "aws_vpc" "vpc" {
 cidr_block = "${var.vpc_cidr}"
 enable_dns_hostnames = "${var.dnshostname}"
@@ -7,7 +7,7 @@ Name = "${var.vpc_name}"
 }
 }
 
-
+/*
 data "aws_availability_zones" "available" {
   state = "available"
 }
