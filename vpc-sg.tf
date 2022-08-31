dc446ae66ee14fcdc44afc01b3312558753825eb
root <root@ip-172-31-33-192.ap-northeast-1.compute.internal>
2022-08-31 10:17:38 +0000

    vpc-sg

--- vpc-sg.tf
+++ vpc-sg.tf
@@ -1,15 +1,14 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.vpc.id
-
+ vpc_id = "${aws_vpc.vpc.id}"
   ingress {
     description      = "TLS from VPC"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = [aws_vpc.vpc.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.vpc.ipv6_cidr_block]
   }
 
   egress {
